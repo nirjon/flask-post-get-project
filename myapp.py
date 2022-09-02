ad91d960b12eb7efecb31b7af041027470bee9c7
Shahriar Nirjon <nirjon@cs.unc.edu>
2022-09-01 21:57:35 -0400

    runs in local host

---
+++ myapp.py
@@ -0,0 +1,20 @@
+from flask import Flask, request
+import json
+
+app = Flask(__name__)
+
+myjokes = ["why did the chicken cross the street?"]
+
+@app.route("/getjoke", methods=["GET"])
+def get_all_joke():
+    x = {"all-jokes" : myjokes}
+    return json.dumps(x)
+
+@app.route("/postjoke", methods=["POST"])
+def post_new_joke():
+    data = request.get_json() #json expected with "joke" key
+    myjokes.append(data["joke"])
+    return data    
+
+if __name__=='__main__':
+    app.run(debug=True)
